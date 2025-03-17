5f34aab5a4e93eecc84f677ec73cb070825c1594
Axel <axel.adolfsson@skola.taby.se>
2025-03-17 12:18:05 +0100

    better movement + grid

--- client/client.js
+++ client/client.js
@@ -2,42 +2,76 @@ var socket = io("http://10.32.45.162:3000");
 
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
+const grid_size = 50;
 
 let s;
 let p;
 let id;
+let player_index;
 
-socket.on("game_init", function (player, state, socket_id) {
+let last_timestamp = 0;
+
+socket.on("game_init", (player, state, socket_id, index) => {
         console.log(player);
         p = player;
         s = state;
         id = socket_id;
+        player_index = index;
         requestAnimationFrame(main);
 });
 
 socket.on("server_upd", (state) => {
         s = state;
-        draw_state(state);
 });
+
+function reset_directions()
+{
+        s.players[player_index].dir.left = false;
+        s.players[player_index].dir.right = false;
+        s.players[player_index].dir.up = false;
+        s.players[player_index].dir.down = false;
+}
 
 window.addEventListener("keydown", (e) => {
         e.preventDefault();
         switch (e.key.toLowerCase()) {
                 case "w":
-                        p.pos.y -= 10;
+                        reset_directions()
+                        s.players[player_index].dir.up = true;
                         break;
                 case "a":
-                        p.pos.x -= 10;
+                        reset_directions();
+                        s.players[player_index].dir.left = true;
                         break;
                 case "s":
-                        p.pos.y += 10;
+                        reset_directions();
+                        s.players[player_index].dir.down = true;
                         break;
                 case "d":
-                        p.pos.x += 10;
+                        reset_directions();
+                        s.players[player_index].dir.right = true;
+                        break;
+                default:
                         break;
         }
-        socket.emit("client_upd", p);
 });
+
+function draw_grid()
+{
+        for (let y = 0; y < c.height; y += grid_size) {
+                ctx.beginPath();
+                ctx.moveTo(0, y);
+                ctx.lineTo(c.width, y);
+                ctx.stroke();
+        }
+        for (let x = 0; x < c.width; x += grid_size) {
+                ctx.beginPath();
+                ctx.moveTo(x, 0);
+                ctx.lineTo(x, c.height);
+                ctx.line_width = 10;
+                ctx.stroke();
+        }
+}
 
 function draw_player(player)
 {
@@ -49,14 +83,40 @@ function draw_player(player)
 
 function draw_state(state)
 {
-        for (let i = 0; i < state.players.length; i++) {
-                draw_player(state.players[i]);
-        }
+        state.players.forEach((player) => {
+                draw_player(player);
+        });
 }
 
+
+const TARGET_FPS = 30;
+const frame_time = 1000 / TARGET_FPS; // Time per frame in milliseconds
+let last_frame_time = 0;
+
+let count = 0;
 function main(timestamp)
 {
-        ctx.clearRect(0, 0, 900, 450);
-        draw_state(s);
+        let dtime = timestamp - last_frame_time;
+
+        if (dtime >= frame_time) { 
+                last_frame_time = timestamp;
+
+                if (last_timestamp > 0) {
+                        let delta = (timestamp - last_timestamp) / 1000;
+                        fps = 1 / delta;
+                }
+                last_timestamp = timestamp;
+
+                // Game loop
+                if (count >= 15) {
+                        socket.emit("client_upd", s, id);
+                        count = 0;
+                }
+                ctx.clearRect(0, 0, 900, 450);
+                draw_grid();
+                draw_state(s);
+        }
+
+        count++;
         requestAnimationFrame(main);
 }
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, { cors: { origin: "*" } });
 
+const grid_size = 50;
+
 app.use(express.static(path.join(__dirname, "client")));
 
 app.get('/', (req, res) => {
@@ -23,10 +25,32 @@ function create_player()
         let p = {
                 id: state.players.length,
                 pos: { x: 0, y: 0 },
-                len: 1
+                dir: {
+                        left: false,
+                        right: false,
+                        up: false,
+                        down: false
+                },
+                len: 1,
+                connected: 1,
         };
         state.players.push(p);
         return p;
+}
+
+function update_positions()
+{
+        for (let i = 0; i < state.players.length; i++) {
+                if (state.players[i].dir.up) {
+                        state.players[i].pos.y -= grid_size;
+                } else if (state.players[i].dir.down) {
+                        state.players[i].pos.y += grid_size;
+                } else if (state.players[i].dir.left) {
+                        state.players[i].pos.x -= grid_size;
+                } else if (state.players[i].dir.right) {
+                        state.players[i].pos.x += grid_size;
+                }
+        }
 }
 
 io.on("connection", (socket) => {
@@ -34,20 +58,18 @@ io.on("connection", (socket) => {
         console.log("a user connected");
 
         let player = create_player();
+        let player_index = player.id;
+
         console.log(player);
-        socket.emit("game_init", player, state, socket_id);
+        socket.emit("game_init", player, state, socket_id, player_index);
 
         socket.on("disconnect", () => {
                 console.log("user disconnected");
         });
 
-        socket.on("client_upd", (p, client_id) => {
-                for (let i = 0; i < state.players.length; i++) {
-                        if (state.players[i].id == p.id) {
-                                state.players[i] = p;
-                                break;
-                        }
-                }
+        socket.on("client_upd", (s, client_id) => {
+                state = s;
+                update_positions();
                 io.emit("server_upd", state);
         });
 });
