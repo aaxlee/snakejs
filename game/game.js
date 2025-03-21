dc77b6dabd2c3179fa34e6d3c21bc49923c726cd
Axel <axel.adolfsson@skola.taby.se>
2025-03-21 12:17:57 +0100

    Implemented map

--- game/game.js
+++ game/game.js
@@ -1,19 +1,26 @@
+const WIDTH = 900;
+const HEIGHT = 450
+
 let state = {
         players: [],
-        grid_size: 50
+        grid_size: 50,
+        width: WIDTH,
+        height: HEIGHT,
 }
+
+let map = [];
 
 function warp_snakes()
 {
         state.players.forEach((player) => {
                 if (player.pos.x < 0) {
-                        player.pos.x = 900;
-                } else if (player.pos.x > 900) {
-                        player.pos.x = -state.grid_size;
+                        player.pos.x = state.width;
+                } else if (player.pos.x > state.width) {
+                        player.pos.x = 0;
                 } else if (player.pos.y < 0) {
-                        player.pos.y = 450;
-                } else if (player.pos.y > 450) {
-                        player.pos.y = -state.grid_size;
+                        player.pos.y = state.height;
+                } else if (player.pos.y > state.height) {
+                        player.pos.y = 0;
                 }
         });
 }
@@ -46,8 +53,35 @@ function update_snakes()
         }
 }
 
+function update_map()
+{
+        const rows = state.height / state.grid_size;
+        const cols = state.width / state.grid_size;
+        for (let y = 0; y < rows; y++) {
+                map[y] = [];
+                for (let x = 0; x < cols; x++) {
+                        map[y][x] = 0;
+                }
+        }
+        state.players.forEach((player) => {
+                map[player.pos.y / state.grid_size][player.pos.x / state.grid_size] = 1;
+                player.tail.forEach((tail) => {
+                        if (tail.x >= 0 && tail.y >= 0) {
+                                map[tail.y / state.grid_size][tail.x / state.grid_size] = 1;
+                        }
+                });
+        });
+}
+
+function generate_food()
+{
+
+}
+
 module.exports = {
         state,
+        map,
         update_snakes,
+        update_map,
         warp_snakes
 }
