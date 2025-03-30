233cefbc735e00fa8d75fa54ba4e51ae2c2052fa
Axel <axel.adolfsson@skola.taby.se>
2025-03-30 17:08:59 +0200

    add title screen support

--- client/main.js
+++ client/main.js
@@ -1,10 +1,17 @@
-import { socket, WIDTH, HEIGHT, c, ctx, grid_size, TARGET_FPS, FRAME_TIME } from "./constants.js";
+import { socket, WIDTH, HEIGHT, join_button, title_screen, canvas_container, c, ctx, grid_size, TARGET_FPS, FRAME_TIME } from "./constants.js";
 import { draw_grid, draw_food, draw_state } from "./rendering.js";
 import "./events.js";
 
 export let s;
 export let id;
 export let player_index = -1;
+
+join_button.addEventListener("click", () => {
+        title_screen.style.display = "none";
+        canvas_container.style.display = "flex";
+
+        socket.emit("join_game");
+});
 
 socket.on("game_init", (state, socket_id) => {
         s = state;
@@ -32,6 +39,7 @@ let last_timestamp = 0;
 let last_frame_time = 0;
 let fps;
 let count = 0;
+
 function main(timestamp)
 {
         let dtime = timestamp - last_frame_time;
