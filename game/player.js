e8c48540f6784ec096e72d3c71072fc1f1834200
Axel <axel.adolfsson@skola.taby.se>
2025-03-31 11:25:46 +0200

    add player class

--- game/player.js
+++ game/player.js
@@ -1,29 +1,71 @@
-function create_player(state, socket_id, color)
-{
-        let p = {
-                id: state.players.length,
-                pos: { x: 0, y: 0 },
-                dir: {
-                        left: false,
-                        right: false,
-                        up: false,
-                        down: false
-                },
-                tail: [
-                        /*
-                        { x: 50, y: 0, dir: {}, entity_type: "tail" },
-                        { x: 100, y: 0, dir: {}, entity_type: "tail" },
-                        { x: 150, y: 0, dir: {}, entity_type: "tail" }
-                        */
-                ],
-                color: color,
-                socket_id: socket_id,
-                connected: 1,
-                entity_type: "player"
-        };
-        return p;
+class Player {
+	constructor(startpos, socket_id, color) {
+		this.pos = startpos;
+		this.dir = {
+			left: false,
+			right: false,
+			up: false,
+			down: false
+		};
+		this.tail = [
+
+		];
+		this.color = color;
+		this.socket_id = socket_id;
+		this.connected = 1;
+		this.entity_type = "player";
+	}
+	update_snake(step_size) {
+		let prev = { ...this.pos, dir: { ...this.dir }, parent_id: this.socket_id, entity_type: "tail" };
+		if (this.dir.up) {
+			this.pos.y -= step_size;
+		} else if (this.dir.down) {
+			this.pos.y += step_size
+		} else if (this.dir.left) {
+			this.pos.x -= step_size;
+		} else if (this.dir.right) {
+			this.pos.x += step_size;
+		}
+
+		if (this.tail.length > 0) {
+			for (let j = 0; j < this.tail.length; j++) {
+				let next = this.tail[j];
+				this.tail[j] = prev;
+				prev = next;
+			}
+		}
+	}
+	extend_tail(step_size) {
+		let end;
+		if (this.tail.length > 0) {
+			end = this.tail[this.tail.length - 1];
+		} else {
+			end = { ...this.pos , dir: { ...this.dir }, parent_id: this.socket_id, entity_type: "tail" };
+		}
+		let new_end = JSON.parse(JSON.stringify(end));
+		if (new_end.dir.up) {
+			new_end.y += step_size;
+		} else if (new_end.dir.down) {
+			new_end.y -= step_size;
+		} else if (new_end.dir.left) {
+			new_end.x += step_size;
+		} else if (new_end.dir.right) {
+			new_end.x -= step_size;
+		}
+
+		this.tail.push(new_end);
+	}
+	warp(state) {
+		if (this.pos.x < 0) {
+			this.pos.x = state.width - state.grid_size;
+		} else if (this.pos.x > state.width - state.grid_size) {
+			this.pos.x = 0;
+		} else if (this.pos.y < 0) {
+			this.pos.y = state.height - state.grid_size;
+		} else if (this.pos.y > state.height - state.grid_size) {
+			this.pos.y = 0;
+		}
+	}
 }
 
-module.exports = {
-        create_player
-}
+module.exports = Player;
